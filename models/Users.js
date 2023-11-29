344364cc944b06b696397c651355d106cafcb3ae
nibin <nbaby@orthofx.com>
2023-11-29 22:34:56 +0530

    mongoose hooks and hashing passwords

--- models/Users.js
+++ models/Users.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const { isEmail } = require('validator');
 
+const bcrypt = require('bcrypt');
+
 const userSchema = new mongoose.Schema({
     email: {
         required: [true, 'please enter an email'],
@@ -16,6 +18,24 @@ const userSchema = new mongoose.Schema({
         minlength: [6, 'minimum password length is 6 characters']
     }
 });
+
+// fire a function after a document is saved to db: mongoose hook
+userSchema.post('save', function(doc, next) { 
+    console.log('document saved to db', doc)
+    // to continue the middleware to cater the request
+    next();
+})
+
+// fire a function before a document is saved to db: mongoose hook
+userSchema.pre('save', async function(next) { 
+    // we will need a 'salt' string to be added to the password and then it is hashed and saved to the db
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    // 'this.' is accessible here, it refers to the model instant called in the doc saving post method
+    console.log('about to be created and saved', this)
+    // to continue the middleware to cater the request
+    next();
+})
 
 const User = mongoose.model('user', userSchema);
 
