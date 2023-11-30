15d756761d588c63b90699e4c6734bb4ba506add
nibin <nbaby@orthofx.com>
2023-11-30 18:19:47 +0530

    login signup view and cookies intro

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const credentials = require('./creds.json');
+const cookie_parser = require('cookie-parser');
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookie_parser())
 
 
 app.set('view engine', 'ejs');
@@ -22,3 +24,23 @@ app.get('/', (req, res) =>res.render('home'));
 app.get('/smoothies', (req, res) =>res.render('smoothies'));
 
 app.use(authRoutes);
+
+
+app.get('/set-cookies', (req, res) =>{
+    // res.setHeader('Set-cookie', 'newUser=true');
+    // the 3rd argument is optional :
+    // it can be maxage for exipiration
+    // secure, for https sites only
+    // httpOnly, to diable the access of cookie through javascript i.e. document.cookie is not gonna give the value
+    res.cookie('newUser', false, {maxAge: 1000*60*60*24, httpOnly: true})
+    res.send('cookies set');
+})
+
+
+
+app.get('/read-cookies', (req, res) =>{
+
+    const cookies = req.cookies;
+    res.json(cookies);
+    
+})
